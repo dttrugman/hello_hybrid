befbb19646d5325bb5245e80459e1a28406b7b7b
Daniel Trugman <dtrugman@ucsd.edu>
2015-08-11 11:21:56 -0700

    added .gitignore

--- hello_hybrid.c
+++ hello_hybrid.c
@@ -14,13 +14,13 @@ int main(int argc, char *argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Get_processor_name(processor_name, &namelen);
 
-  printf("Hello world from process %d", rank);
+  printf("hello world from process %d", rank);
 
   #pragma omp parallel default(shared) private(iam, np)
   {
     np = omp_get_num_threads();
     iam = omp_get_thread_num();
-    printf("Hello world from thread %d out of %d from process %d out of %d on %s\n",
+    printf("hello world from thread %d out of %d from process %d out of %d on %s\n",
            iam, np, rank, numprocs, processor_name);
   }
 
